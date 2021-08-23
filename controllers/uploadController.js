d6b3ed6212b91711c7815ea7cf4b2de7b2f40aa9
Ivan Mercedes <ivanmercede@gmail.com>
2021-08-22 21:51:29 -0400

    Validate file extension

--- controllers/uploadController.js
+++ controllers/uploadController.js
@@ -9,15 +9,28 @@ const uploadFile = (req, res = response) => {
 
   const { file } = req.files;
 
-  const uploadPath = path.join(__dirname, "../uploads/", file.name);
+  const nombreCortado = file.name.split(".");
+  const extension = nombreCortado[nombreCortado.length - 1];
 
-  file.mv(uploadPath, function (err) {
-    if (err) {
-      return res.status(500).json({ err });
-    }
+  // Validar la extesion
+  const extesionesValidas = ["png", "jpg", "jpeg", "gif"];
 
-    res.json({ msg: "Archivo subido a " + uploadPath });
-  });
+  if (!extesionesValidas.includes(extension)) {
+    return res.status(400).json({
+      msg: `La extesion ${extension} no es permitida, por favor prueba con otro archivo que tenga la extensiones ${extesionesValidas}`,
+    });
+  }
+  console.log(extension);
+
+  //   const uploadPath = path.join(__dirname, "../uploads/", file.name);
+
+  //   file.mv(uploadPath, function (err) {
+  //     if (err) {
+  //       return res.status(500).json({ err });
+  //     }
+
+  //     res.json({ msg: "Archivo subido a " + uploadPath });
+  //   });
 };
 
 module.exports = {
